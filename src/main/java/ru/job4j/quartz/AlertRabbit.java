ac9162451bbc64442002098ea6a2aa1c4de9f5ca
Tsypa90 <URSA2004@mail.ru>
2022-05-16 10:41:45 +0300

    1.1. Job c параметрами [#260360 #301475]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -9,53 +9,69 @@ import static org.quartz.JobBuilder.*;
 import static org.quartz.TriggerBuilder.*;
 import static org.quartz.SimpleScheduleBuilder.*;
 
-public class AlertRabbit implements AutoCloseable {
-    private static Connection cn;
+public class AlertRabbit {
 
-     public static void main(String[] args) {
-        try (InputStream in = AlertRabbit.class
-                .getClassLoader().getResourceAsStream("app.properties")) {
-            Properties config = new Properties();
-            config.load(in);
-            Class.forName(config.getProperty("driver-class-name"));
-            cn = DriverManager.getConnection(
-                    config.getProperty("url"),
-                    config.getProperty("username"),
-                    config.getProperty("password"));
-            Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-            scheduler.start();
-            JobDetail job = newJob(Rabbit.class)
-                    .build();
-            SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(5)
-                    .repeatForever();
-            Trigger trigger = newTrigger()
-                    .startNow()
-                    .withSchedule(times)
-                    .build();
-            scheduler.scheduleJob(job, trigger);
-            Thread.sleep(10000);
-            scheduler.shutdown();
+    private static int readProperties() {
+        int time = -1;
+        try (InputStream in = AlertRabbit.class.getClassLoader()
+                .getResourceAsStream("rabbit.properties")) {
+            Properties properties = new Properties();
+            properties.load(in);
+            if (properties.getProperty("rabbit.interval").isEmpty()) {
+                throw new IllegalArgumentException("Properties not valid!");
+            }
+            time = Integer.parseInt(properties.getProperty("rabbit.interval"));
         } catch (Exception e) {
             e.printStackTrace();
         }
+        return time;
     }
 
-    @Override
-    public void close() throws Exception {
-        cn.close();
+     public static void main(String[] args) {
+        try {
+            try (InputStream in = AlertRabbit.class
+                    .getClassLoader().getResourceAsStream("app.properties")) {
+                Properties config = new Properties();
+                config.load(in);
+                Class.forName(config.getProperty("driver-class-name"));
+                Connection cn = DriverManager.getConnection(
+                        config.getProperty("url"),
+                        config.getProperty("username"),
+                        config.getProperty("password"));
+                Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
+                scheduler.start();
+                JobDataMap data = new JobDataMap();
+                data.put("connection", cn);
+                JobDetail job = newJob(Rabbit.class)
+                        .usingJobData(data)
+                        .build();
+                SimpleScheduleBuilder times = simpleSchedule()
+                        .withIntervalInSeconds(readProperties())
+                        .repeatForever();
+                Trigger trigger = newTrigger()
+                        .startNow()
+                        .withSchedule(times)
+                        .build();
+                scheduler.scheduleJob(job, trigger);
+                Thread.sleep(10000);
+                scheduler.shutdown();
+            }
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
     }
 
     public static class Rabbit implements Job {
-        private Connection cn = AlertRabbit.cn;
 
         public Rabbit() {
         }
 
         @Override
         public void execute(JobExecutionContext context) throws JobExecutionException {
+
             try (PreparedStatement statement =
-                         cn.prepareStatement("insert into rabbit(created_date) values(?)")) {
+                         ((Connection) context.getJobDetail().getJobDataMap().get("connection"))
+                                 .prepareStatement("insert into rabbit(created_date) values(?)")) {
                 statement.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
                 statement.execute();
             } catch (SQLException sqlException) {
