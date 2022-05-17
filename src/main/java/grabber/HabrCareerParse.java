ecffadbf20a9570314bb7c06f700e0c9bccf16cb
Tsypa90 <URSA2004@mail.ru>
2022-05-17 17:35:42 +0300

    2.3. Загрузка деталей поста. [#285212]

--- src/main/java/grabber/HabrCareerParse.java
+++ src/main/java/grabber/HabrCareerParse.java
@@ -14,8 +14,12 @@ public class HabrCareerParse {
 
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
 
+    private static String retrieveDescription(String link) throws IOException {
+        return Jsoup.connect(link).get().selectFirst(".job_show_description__vacancy_description").text();
+    }
+
     public static void main(String[] args) throws IOException {
-        for (int page = 1; page <= 5; page++) {
+        for (int page = 1; page <= 1; page++) {
             Connection connection = Jsoup.connect(String.format("%s%s", PAGE_LINK, page));
             Document document = connection.get();
             Elements rows = document.select(".vacancy-card__inner");
