59350899d947c402b51afa141b099f7234fd2e0e
Tsypa90 <URSA2004@mail.ru>
2022-05-19 12:25:08 +0300

    5. PsqlStore [#285209 #302404]

--- src/main/java/grabber/PsqlStore.java
+++ src/main/java/grabber/PsqlStore.java
@@ -42,19 +42,23 @@ public class PsqlStore implements Store, AutoCloseable {
         }
     }
 
+    private Post newPost(ResultSet resultSet) throws SQLException {
+        return new Post(
+                resultSet.getInt("id"),
+                resultSet.getString("name"),
+                resultSet.getString("link"),
+                resultSet.getString("text"),
+                resultSet.getTimestamp("created").toLocalDateTime()
+        );
+    }
+
     @Override
     public List<Post> getAll() {
         List<Post> posts = new ArrayList<>();
         try (PreparedStatement statement = cn.prepareStatement("select * from post")) {
             try (ResultSet resultSet = statement.executeQuery()) {
                 while (resultSet.next()) {
-                    posts.add(new Post(
-                            resultSet.getInt("id"),
-                            resultSet.getString("name"),
-                            resultSet.getString("link"),
-                            resultSet.getString("text"),
-                            resultSet.getTimestamp("created").toLocalDateTime()
-                            ));
+                    posts.add(newPost(resultSet));
                 }
             }
         } catch (SQLException e) {
@@ -65,7 +69,17 @@ public class PsqlStore implements Store, AutoCloseable {
 
     @Override
     public Post findById(int id) {
-        return getAll().stream().filter(post -> post.getId() == id).findFirst().get();
+        Post post = null;
+        try (PreparedStatement statement = cn.prepareStatement(String.format("select * from post where id =%s", id))) {
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (resultSet.next()) {
+                    post = newPost(resultSet);
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return post;
     }
 
     @Override
@@ -79,14 +93,13 @@ public class PsqlStore implements Store, AutoCloseable {
         try (InputStream in = PsqlStore.class.getClassLoader().getResourceAsStream("app.properties")) {
             Properties config = new Properties();
             config.load(in);
-            for (int page = 1; page <= 5; page++) {
-                var posts = new HabrCareerParse(new HarbCareerDateTimeParser())
-                        .list(String.format("https://career.habr.com/vacancies/java_developer?page=%s", page));
-                posts.stream().forEach(post -> new PsqlStore(config).save(post));
-            }
-            List<Post> sqlValues = new PsqlStore(config).getAll();
+            PsqlStore psqlStore = new PsqlStore(config);
+            var posts = new HabrCareerParse(new HarbCareerDateTimeParser())
+                        .list("https://career.habr.com/vacancies/java_developer?page=");
+            posts.stream().forEach(post -> psqlStore.save(post));
+            var sqlValues = psqlStore.getAll();
             sqlValues.stream().forEach(System.out::println);
-            System.out.println(new PsqlStore(config).findById(48));
+            System.out.println(psqlStore.findById(48));
         } catch (IOException e) {
             e.printStackTrace();
         }
