7ef483ec85bf9626f191a5285f5713a47290bca8
Tsypa90 <URSA2004@mail.ru>
2022-05-19 19:51:46 +0300

    6. Grabber. [#289477 #302807]

--- src/main/java/grabber/Grabber.java
+++ src/main/java/grabber/Grabber.java
@@ -14,6 +14,8 @@ import static org.quartz.TriggerBuilder.newTrigger;
 public class Grabber implements Grab {
     private final Properties cfg = new Properties();
 
+    public static final String PAGE_LINK = "https://career.habr.com/vacancies/java_developer?page=";
+
     public Store store() {
         return new PsqlStore(cfg);
     }
@@ -55,7 +57,7 @@ public class Grabber implements Grab {
             JobDataMap map = context.getJobDetail().getJobDataMap();
             Store store = (Store) map.get("store");
             Parse parse = (Parse) map.get("parse");
-            parse.list("https://career.habr.com/vacancies/java_developer?page=")
+            parse.list(PAGE_LINK)
                     .forEach(store::save);
         }
     }
