8f89dec5c2682d1fed19cb703da329772f5bc7fa
Tsypa90 <URSA2004@mail.ru>
2022-06-05 12:05:29 +0300

    1. Отчеты. [#850 #308950]

--- src/main/java/srp/ProgramReport.java
+++ src/main/java/srp/ProgramReport.java
@@ -1,26 +1,33 @@
 package srp;
 
+import java.util.Calendar;
 import java.util.function.Predicate;
 
-public class AccountingReport implements Report {
+public class ProgramReport implements Report {
     private Store store;
-    public static final int USD = 60;
 
-    public AccountingReport(MemStore store) {
+    public ProgramReport(Store store) {
         this.store = store;
     }
 
     @Override
     public String generate(Predicate<Employee> filter) {
         StringBuilder text = new StringBuilder();
-        text.append("Name; Hired; Fired; Salary in RUB;").append(System.lineSeparator());
+        text.append("<html lang=\"ru\">\n")
+                .append("<head>\n")
+                .append("<meta charset=\"UTF-8\">\n")
+                .append("<title>Report for department of programmers</title>\n")
+                .append("</head>\n")
+                .append("<body>\n");
         for (Employee employee : store.findBy(filter)) {
             text.append(employee.getName()).append(";")
                     .append(employee.getHired()).append(";")
                     .append(employee.getFired()).append(";")
-                    .append(employee.getSalary() * USD).append(";")
+                    .append(employee.getSalary()).append(";")
                     .append(System.lineSeparator());
         }
+        text.append("</body>\n")
+                .append("</html>\n");
         return text.toString();
     }
 }
--- src/test/java/srp/ProgramReportTest.java
+++ src/test/java/srp/ProgramReportTest.java
@@ -7,22 +7,28 @@ import java.util.Calendar;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-public class AccountingReportTest {
+public class ProgramReportTest {
     @Test
-    public void whenAccountingGenerated() {
+    public void whenProgramGenerated() {
         MemStore store = new MemStore();
         Calendar now = Calendar.getInstance();
         Employee worker = new Employee("Ivan", now, now, 100);
         store.add(worker);
-        Report engine = new AccountingReport(store);
+        Report engine = new ProgramReport(store);
         StringBuilder expect = new StringBuilder()
-                .append("Name; Hired; Fired; Salary in RUB;")
-                .append(System.lineSeparator())
+                .append("<html lang=\"ru\">\n")
+                .append("<head>\n")
+                .append("<meta charset=\"UTF-8\">\n")
+                .append("<title>Report for department of programmers</title>\n")
+                .append("</head>\n")
+                .append("<body>\n")
                 .append(worker.getName()).append(";")
                 .append(worker.getHired()).append(";")
                 .append(worker.getFired()).append(";")
-                .append(worker.getSalary() * 60).append(";")
-                .append(System.lineSeparator());
+                .append(worker.getSalary()).append(";")
+                .append(System.lineSeparator())
+                .append("</body>\n")
+                .append("</html>\n");
         assertThat(engine.generate(em -> true), is(expect.toString()));
     }
 }
