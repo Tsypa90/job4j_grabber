8acdefb449e82bc65a149dd55830f02bb6aee1d1
Tsypa90 <URSA2004@mail.ru>
2022-06-12 12:30:22 +0300

    1. Создать меню. [#4748 #311529]

--- src/test/java/isp/menu/SimpleMenuTest.java
+++ src/test/java/isp/menu/SimpleMenuTest.java
@@ -1,7 +1,11 @@
 package isp.menu;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
@@ -10,6 +14,12 @@ import static org.junit.Assert.*;
 public class SimpleMenuTest {
 
     public static final ActionDelegate STUB_ACTION = System.out::println;
+    private ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(output));
+    }
 
     @Test
     public void whenAddThenReturnSame() {
@@ -54,5 +64,21 @@ public class SimpleMenuTest {
         Menu menu = new SimpleMenu();
         menu.add(Menu.ROOT, "Сходить в магазин", STUB_ACTION);
         menu.select("купить продукты");
+    }
+    @Test
+    public void whenPrintMenu() {
+        Menu menu = new SimpleMenu();
+        menu.add(Menu.ROOT, "Сходить в магазин", STUB_ACTION);
+        menu.add("Сходить в магазин", "Купить продукты", STUB_ACTION);
+        menu.add("Купить продукты", "Купить хлеб", STUB_ACTION);
+        SimpleMenuPrinter printer = new SimpleMenuPrinter();
+        printer.print(menu);
+        assertEquals("1.Сходить в магазин\n"
+                + "    1.1.Купить продукты\n"
+                + "        1.1.1.Купить хлеб\n", output.toString());
+    }
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
     }
 }
