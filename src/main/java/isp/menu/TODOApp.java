4676b5203ac3119e80c403ef77d74b062130e3eb
Tsypa90 <URSA2004@mail.ru>
2022-06-12 12:17:39 +0300

    1. Создать меню. [#4748 #311529]

--- src/main/java/isp/menu/TODOApp.java
+++ src/main/java/isp/menu/TODOApp.java
@@ -6,10 +6,10 @@ public class TODOApp {
     public static void main(String[] args) {
         Menu menu = new SimpleMenu();
         menu.add(Menu.ROOT, "Сходить в магазин", STUB_ACTION);
-//        menu.add(Menu.ROOT, "Покормить собаку", STUB_ACTION);
+        menu.add(Menu.ROOT, "Покормить собаку", STUB_ACTION);
         menu.add("Сходить в магазин", "Купить продукты", STUB_ACTION);
         menu.add("Купить продукты", "Купить хлеб", STUB_ACTION);
-//        menu.add("Купить продукты", "Купить молоко", STUB_ACTION);
+        menu.add("Купить продукты", "Купить молоко", STUB_ACTION);
         new SimpleMenuPrinter().print(menu);
     }
 }
